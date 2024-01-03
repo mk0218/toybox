4215452130d32355f59cb4849d23667bdb5e2ba4
mkkim <mk0218@postech.ac.kr>
2024-01-03 23:35:47 +0900

    Add tree base.

--- treecomponent/src/lib.rs
+++ treecomponent/src/lib.rs
@@ -1,8 +1,50 @@
-use leptos::{component, IntoView, view};
+use leptos::*;
+
+#[derive(Clone)]
+struct TreeNodeInfo {
+    label: String,
+    children: Vec<RwSignal<TreeNodeInfo>>
+}
+
+impl TreeNodeInfo {
+    fn new(label: &str, children: Vec<RwSignal<TreeNodeInfo>>) -> Self {
+        TreeNodeInfo { label: String::from(label), children }
+    }
+}
+
+#[component]
+fn TreeNode(node: TreeNodeInfo) -> impl IntoView {
+    let children: Vec<_> = node.children.iter().map(|n| view! {
+        <TreeNode node={n.get()} />
+    }).collect();
+
+    view! {
+        <div>
+            {node.label}
+            {children}
+        </div>
+    }
+}
+
+#[component]
+fn Tree(nodes: Vec<RwSignal<TreeNodeInfo>>) -> impl IntoView {
+    let children: Vec<_> = nodes.iter().map(|n| view! {
+        <TreeNode node={n.get()} />
+    }).collect();
+
+    view! {
+        <div>{children}</div>
+    }
+}
 
 #[component]
 pub fn App() -> impl IntoView {
+    let tree = vec![
+        RwSignal::new(TreeNodeInfo::new("1", vec![RwSignal::new(TreeNodeInfo::new("1-1", vec![]))])),
+        RwSignal::new(TreeNodeInfo::new("2", vec![])),
+        RwSignal::new(TreeNodeInfo::new("3", vec![])),
+    ];
     view! {
-        <p>"Hello, world!"</p>
+        <Tree nodes={tree} />
     }
 }
