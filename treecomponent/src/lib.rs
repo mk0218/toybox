b69eadeb437ba963f65ab949a0e2c736e83d3153
mkkim <mk0218@postech.ac.kr>
2024-01-03 22:42:14 +0900

    Initial commit

---
+++ treecomponent/src/lib.rs
@@ -0,0 +1,8 @@
+use leptos::{component, IntoView, view};
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <p>"Hello, world!"</p>
+    }
+}
---
+++ treecomponent/src/main.rs
@@ -0,0 +1,6 @@
+use leptos::{mount_to_body, view};
+use treecomponent::App;
+
+fn main() {
+    mount_to_body(|| view! { <App /> })
+}
